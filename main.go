8ffddc3c3be2ee0ea4bc135a1f2d9f83f433ca23
Diogo Correia <diogorodriguescorreia89@gmail.com>
2024-11-01 18:38:31 +0000

    Integrate gorm.

--- main.go
+++ main.go
@@ -3,14 +3,58 @@ package main
 import (
 	"fmt"
 	"github.com/diogorodriguesc/lutetium-go"
+	"golang.org/x/net/html"
 	"gopkg.in/yaml.v2"
+	"net/http"
 	"os"
+	"strings"
 	"sync"
 
 	_ "github.com/lib/pq"
 )
 
-func printValues(XmlFile XmlFile, wg *sync.WaitGroup) {
+func GetMetaRobotsContent(res *http.Response) string {
+	doc, err := html.Parse(res.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	var robotsContent string
+	var f func(*html.Node)
+	f = func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "meta" {
+			// Check if the meta tag has name="robots"
+			var nameAttr, contentAttr string
+			for _, attr := range n.Attr {
+				if attr.Key == "name" && strings.ToLower(attr.Val) == "robots" {
+					nameAttr = attr.Val
+				}
+				if attr.Key == "content" {
+					contentAttr = attr.Val
+				}
+			}
+			// If found, set robotsContent
+			if nameAttr == "robots" {
+				robotsContent = contentAttr
+				return
+			}
+		}
+		// Recursively apply to children
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			f(c)
+		}
+	}
+
+	f(doc)
+
+	if robotsContent == "" {
+		fmt.Errorf("robots meta tag not found")
+	}
+
+	return robotsContent
+}
+
+func ProcessXmlFile(XmlFile XmlFile, wg *sync.WaitGroup) {
 	defer wg.Done()
 
 	fmt.Printf("%+v\n", XmlFile)
@@ -24,7 +68,18 @@ func printValues(XmlFile XmlFile, wg *sync.WaitGroup) {
 	UrlSet := f.Read()
 
 	for _, c := range UrlSet.Urls {
-		fmt.Fprintf(os.Stdout, "%s\n", c.Loc)
+		requestUrl := fmt.Sprintf(c.Loc)
+		res, err := http.Get(requestUrl)
+
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		defer res.Body.Close()
+
+		robotsContent := GetMetaRobotsContent(res)
+
+		fmt.Printf("url: %s client: status code: %d robots: %s\n", c.Loc, res.StatusCode, robotsContent)
 	}
 }
 
@@ -53,7 +108,7 @@ func main() {
 	wg.Add(len(files))
 
 	for _, XmlFile := range files {
-		go printValues(XmlFile, &wg)
+		go ProcessXmlFile(XmlFile, &wg)
 	}
 
 	wg.Wait()
